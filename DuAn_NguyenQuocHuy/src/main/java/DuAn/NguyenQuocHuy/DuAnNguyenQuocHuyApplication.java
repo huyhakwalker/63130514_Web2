045a58b2e640f3b37f844e4987658aac8ced552b
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2024-04-16 15:04:41 +0700

    DuAn

---
+++ DuAn_NguyenQuocHuy/src/main/java/DuAn/NguyenQuocHuy/DuAnNguyenQuocHuyApplication.java
@@ -0,0 +1,13 @@
+package DuAn.NguyenQuocHuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuAnNguyenQuocHuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuAnNguyenQuocHuyApplication.class, args);
+	}
+
+}
---
+++ DuAn_NguyenQuocHuy/src/test/java/DuAn/NguyenQuocHuy/DuAnNguyenQuocHuyApplicationTests.java
@@ -0,0 +1,13 @@
+package DuAn.NguyenQuocHuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnNguyenQuocHuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
