93f319056fcfcc760aa650d718d8481c25501906
Nguyễn Quốc Huy <huyhakwalker@gmail.com>
2024-05-03 14:40:38 +0700

    update something

--- DuAn_NguyenQuocHuy/src/main/java/DuAn/NguyenQuocHuy/controller/HomeController.java
+++ DuAn_NguyenQuocHuy/src/main/java/DuAn/NguyenQuocHuy/controller/HomeController.java
@@ -1,5 +1,22 @@
 package DuAn.NguyenQuocHuy.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/dish_register")
+	public String dishRegister() {
+		return "dishRegister";
+	}
+	
+	@GetMapping("/available_dish")
+	public String getAllDish() {
+		return "dishList";
+	}
 }
